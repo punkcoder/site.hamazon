4ee5ac04a49e53b40081277041c33ec727c72c50
James McKee <james.mckee@punk-coder.com>
2016-10-20 22:59:04 -0600

    Some work on the products and changes around javascript

---
+++ Hamazon/Controllers/ApiCartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using UCommerce.Api;
+using UCommerce.EntitiesV2;
+using UCommerce.Runtime;
+using Umbraco.Web.WebApi;
+
+namespace Hamazon.Controllers
+{
+    public class CartController: UmbracoApiController
+    {
+
+        [HttpPost]
+        public void AddToCart(String Sku)
+        {
+            TransactionLibrary.AddToBasket(1, Sku);
+        }
+    }
+}
---
+++ Hamazon/js/site.js
@@ -0,0 +1,8 @@
+﻿
+jQuery(document).load()
+
+function addSku(sku) {
+    jQuery.post('/Umbraco/Api/Cart/AddToCart?sku=' + sku, null, function success() {
+        console.log("Added to Cart");
+    });
+}
